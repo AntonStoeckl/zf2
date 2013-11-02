78277111ec452293fe1f934273d2f7b4e2c36c4f
AntonStoeckl <anton@stoeckl.de>
2013-11-02 16:26:00 +0100

    added missing setters and getters

--- library/Zend/Cache/Storage/Adapter/MongoDBOptions.php
+++ library/Zend/Cache/Storage/Adapter/MongoDBOptions.php
@@ -146,4 +146,136 @@ class MongoDBOptions extends AdapterOptions
     {
         return $this->getResourceManager()->getCollection($this->getResourceId());
     }
+
+    public function setConnectTimeoutMS($connectTimeoutMS)
+    {
+        $this->getResourceManager()->setConnectTimeoutMS($this->getResourceId(), $connectTimeoutMS);
+
+        return $this;
+    }
+
+    public function getConnectTimeoutMS()
+    {
+        return $this->getResourceManager()->getConnectTimeoutMS($this->getResourceId());
+    }
+
+    public function setFsync($fsync)
+    {
+        $this->getResourceManager()->setFsync($this->getResourceId(), $fsync);
+
+        return $this;
+    }
+
+    public function getFsync()
+    {
+        return $this->getResourceManager()->getFsync($this->getResourceId());
+    }
+
+    public function setJournal($journal)
+    {
+        $this->getResourceManager()->setJournal($this->getResourceId(), $journal);
+
+        return $this;
+    }
+
+    public function getJournal()
+    {
+        return $this->getResourceManager()->getJournal($this->getResourceId());
+    }
+
+    public function setUsername($username)
+    {
+        $this->getResourceManager()->setUsername($this->getResourceId(), $username);
+
+        return $this;
+    }
+
+    public function getUsername()
+    {
+        return $this->getResourceManager()->getUsername($this->getResourceId());
+    }
+
+    public function setPassword($password)
+    {
+        $this->getResourceManager()->setPassword($this->getResourceId(), $password);
+
+        return $this;
+    }
+
+    public function getPassword()
+    {
+        return $this->getResourceManager()->getPassword($this->getResourceId());
+    }
+
+    public function setReadPreference($readPreference)
+    {
+        $this->getResourceManager()->setReadPreference($this->getResourceId(), $readPreference);
+
+        return $this;
+    }
+
+    public function getReadPreference()
+    {
+        return $this->getResourceManager()->getReadPreference($this->getResourceId());
+    }
+
+    public function setReadPreferenceTags($readPreferenceTags)
+    {
+        $this->getResourceManager()->setReadPreferenceTags($this->getResourceId(), $readPreferenceTags);
+
+        return $this;
+    }
+
+    public function getReadPreferenceTags()
+    {
+        return $this->getResourceManager()->getReadPreferenceTags($this->getResourceId());
+    }
+
+    public function setSocketTimeoutMS($socketTimeoutMS)
+    {
+        $this->getResourceManager()->setSocketTimeoutMS($this->getResourceId(), $socketTimeoutMS);
+
+        return $this;
+    }
+
+    public function getSocketTimeoutMS()
+    {
+        return $this->getResourceManager()->getSocketTimeoutMS($this->getResourceId());
+    }
+
+    public function setSsl($ssl)
+    {
+        $this->getResourceManager()->setSsl($this->getResourceId(), $ssl);
+
+        return $this;
+    }
+
+    public function getSsl()
+    {
+        return $this->getResourceManager()->getSsl($this->getResourceId());
+    }
+
+    public function setW($w)
+    {
+        $this->getResourceManager()->setW($this->getResourceId(), $w);
+
+        return $this;
+    }
+
+    public function getW()
+    {
+        return $this->getResourceManager()->getW($this->getResourceId());
+    }
+
+    public function setWTimeoutMS($wTimeoutMS)
+    {
+        $this->getResourceManager()->setWTimeoutMS($this->getResourceId(), $wTimeoutMS);
+
+        return $this;
+    }
+
+    public function getWTimeoutMS()
+    {
+        return $this->getResourceManager()->getWTimeoutMS($this->getResourceId());
+    }
 }

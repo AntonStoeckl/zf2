cb763857dbae355af59b142c153d0c336417be1e
AntonStoeckl <anton@stoeckl.de>
2013-11-17 17:10:48 +0100

    added testGetSetUsername()

--- tests/ZendTest/Cache/Storage/Adapter/MongoDBTest.php
+++ tests/ZendTest/Cache/Storage/Adapter/MongoDBTest.php
@@ -208,6 +208,17 @@ class MongoDBTest extends CommonAdapterTest
         $this->assertEquals($databaseName, $resourceManager->getDb($this->_options->getResourceId()), 'Incorrect database was returned');
     }
 
+    public function testGetSetUsername()
+    {
+        $user = 'super user';
+        $this->_options->getResourceManager()->setUsername($this->_options->getResourceId(), $user);
+        $this->assertEquals(
+            $user,
+            $this->_options->getResourceManager()->getUsername($this->_options->getResourceId()),
+            'Username was not correctly set'
+        );
+    }
+
     public function testGetSetPassword()
     {
         $pass = 'super secret';
@@ -219,50 +230,8 @@ class MongoDBTest extends CommonAdapterTest
         );
     }
 
-    public function testGetSetLibOptionsOnExistingRedisResourceInstance()
-    {
-        $this->markTestSkipped("Skipped by FooBar");
 
-        $options = array('serializer', RedisResource::SERIALIZER_PHP);
-        $this->_options->setLibOptions($options);
-
-        $value  = array('value');
-        $key    = 'key';
-        //test if it's still possible to set/get item and if lib serializer works
-        $this->_storage->setItem($key, $value);
-        $this->assertEquals($value, $this->_storage->getItem($key), 'Redis should return an array, lib options were not set correctly');
-
-
-        $options = array('serializer', RedisResource::SERIALIZER_NONE);
-        $this->_options->setLibOptions($options);
-        $this->_storage->setItem($key, $value);
-        //should not serialize array correctly
-        $this->assertFalse(is_array($this->_storage->getItem($key)), 'Redis should not serialize automatically anymore, lib options were not set correctly');
-    }
-
-    public function testGetSetLibOptionsWithCleanRedisResourceInstance()
-    {
-        $this->markTestSkipped("Skipped by FooBar");
-
-        $options = array('serializer', RedisResource::SERIALIZER_PHP);
-        $this->_options->setLibOptions($options);
-
-        $redis = new Cache\Storage\Adapter\Redis($this->_options);
-        $value  = array('value');
-        $key    = 'key';
-        //test if it's still possible to set/get item and if lib serializer works
-        $redis->setItem($key, $value);
-        $this->assertEquals($value, $redis->getItem($key), 'Redis should return an array, lib options were not set correctly');
-
-
-        $options = array('serializer', RedisResource::SERIALIZER_NONE);
-        $this->_options->setLibOptions($options);
-        $redis->setItem($key, $value);
-        //should not serialize array correctly
-        $this->assertFalse(is_array($redis->getItem($key)), 'Redis should not serialize automatically anymore, lib options were not set correctly');
-    }
-
-    /* RedisOptions */
+    /* MongoDBOptions */
 
     public function testGetSetResourceManager()
     {
